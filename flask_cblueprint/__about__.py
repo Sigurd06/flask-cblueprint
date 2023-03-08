1ae321cfce9ac2d4e984a2d97805376c2038c438
Camilo Andres Patron Vargas <camilo.patron@glyku.com>
2023-03-08 17:34:46 -0500

    :sparkles: deploy

--- flask_cblueprint/__about__.py
+++
@@ -1,9 +0,0 @@
-# A way to define the metadata of the package.
-__title__ = 'Flask-CBlueprint'
-__version_info__ = ("1", "0", "0")
-__version__ = ".".join(__version_info__)
-__author__ = "Camilo Vargas"
-__author_email__ = "bomilo66@gmail.com"
-__maintainer__ = "Camilo Vargas"
-__license__ = "MIT"
-__copyright__ = "(c) 2023 by Camilo Vargas"
--- setup.py
+++ setup.py
@@ -1,26 +1,17 @@
 from setuptools import setup
 
-
-from flask_cblueprint import __title__, __author__, __author_email__, __version__, __copyright__, __license__
-
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 # Opening the README.md file and assigning it to the variable `readme`.
 readme = open("./README.md", "r")
 
 # A setup function that is used to install the package.
 setup(
-    name=__title__,
-    version= __version__,
-    author= __author__,
-    author_email= __author_email__,
-    license= __license__,
-    copyright= __copyright__,
-    url='https://github.com/Sigurd06/flask-cblueprint/tree/main',
-    download_url='https://github.com/Sigurd06/flask-cblueprint/tree/v0.1.0',
-    long_description=readme.read(),
-    long_description_content_type='text/markdown',
-    keywords=['Flask'],
-    install_requires=required
+    name="Flask-CBlueprint",
+    install_requires=[
+        "Flask>=1.0.4",
+        "Werkzeug>=1.0.1",
+        "click>=8.1.3",
+        "MarkupSafe>=2.1.2",
+        "python-dotenv>=1.0.0",
+        "simple-term-menu>=1.6.1",
+    ],
 )
